85ce01df595f269b0730db28be836df94f3ab039
Giovanni Nero <giovanni.nero@yandex.ru>
2015-01-21 20:32:31 +0300

    Add script and readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Getting-And-Cleaning-Data-Course-Project
+# Getting And Cleaning Data - Course Project
+## Introduction
+This repo contains my course project to [Coursera](www.coursera.org) 
+[“Getting and Cleaning Data”](https://www.coursera.org/course/getdata) course, part of the 
+[Data Science](https://www.coursera.org/specialization/jhudatascience/1) specialization.
+
+There is just one R script called run_analysis.R that extracts from the 
+[Human Activity Recognition Using Smartphones Data Set](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones) [1] 
+only the measurements on the mean and standard deviation for each measurement from initial data and transforms this data into 
+the table containing average of selected variables for each activity and each subject.
+
+For more information about the initial and the final data read CodeBook.md.
+## How to run
+* clon this repo
+* download the initial dataset and copy `UCI HAR Dataset` to the repo directory
+* in R console set the working directory to the repo directory with `setwd("path-to-repo-directory")` command
+* use `source("run_analysis.R")` to run the script
+
+Final table will be saved as `average_data.txt`
+## Files
+* `run_analysis.R` contains the code to perform the data transformation
+* `CodeBook.md` describes raw and tidy data and the transformations that was performed to clean up the raw data
+* `average_data.txt` contains the clean data derived by script executing
+
+
+---
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on 
+Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living 
+(IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
---
+++ run_analysis.R
@@ -0,0 +1,50 @@
+  input.files <- c("UCI HAR Dataset/activity_labels.txt",      #1
+                   "UCI HAR Dataset/features.txt",             #2
+                   "UCI HAR Dataset/test/subject_test.txt",    #3
+                   "UCI HAR Dataset/test/X_test.txt",          #4
+                   "UCI HAR Dataset/test/y_test.txt",          #5
+                   "UCI HAR Dataset/train/subject_train.txt",  #6
+                   "UCI HAR Dataset/train/X_train.txt",        #7
+                   "UCI HAR Dataset/train/y_train.txt")        #8
+  # reading files
+  activityLabels <- read.table(input.files[1], stringsAsFactors = FALSE, 
+                               col.names = c("id", "activity"))
+  features <- read.table(input.files[2], stringsAsFactors = FALSE)
+  subjectTest <- read.table(input.files[3])
+  subjectTrain <- read.table(input.files[6])
+  activityTest <- read.table(input.files[5])
+  activityTrain <- read.table(input.files[8])
+  xTest <- read.table(input.files[4])
+  xTrain <- read.table(input.files[7])
+  # creating raw table
+  rawData <- rbind(
+               cbind(activityTest, subjectTest, xTest),
+               cbind(activityTrain, subjectTrain, xTrain))
+  rm(subjectTest, subjectTrain, activityTest, activityTrain, xTest, xTrain)
+  names(rawData) <- c("id", "subject", features[,2])
+  rawData <- rawData[, grep("mean\\(|std|subject|id", names(rawData))]
+  # making tidy names for activities
+  activityLabels$activity <- gsub("_", "",
+                               gsub("(_|^)([[:alpha:]])", "\\1\\U\\2",
+                                 tolower(activityLabels$activity),
+                                 perl = TRUE))
+  rawData <- merge(rawData, activityLabels, by.x = "id", by.y = "id")[, -1]
+  # making descriptive names for features
+  names(rawData) <- gsub("BodyBody", "Body",  
+                      gsub("Gyro", "AngularVelocity",
+                        gsub("GyroJerk", "AngularAcceleration",
+                          gsub("Acc", "Acceleration",
+                            gsub("std","Std",
+                              gsub("mean", "Mean",
+                                gsub("\\(|\\)|,|-", "",
+                                  gsub("^f", "Freq",
+                                    gsub("^t", "Time",
+                                      names(rawData))))))))))
+  # making tidy table
+  tidyData <- aggregate(rawData[, 2:ncol(rawData)-1],
+                by = list(Subject = rawData$subject, Activity = rawData$activity),
+                mean)
+  tidyData <- tidyData[, -grep("subject|activity", names(tidyData))]
+  tidyData <- tidyData[, c(2,1,3:ncol(tidyData))]
+  write.table(tidyData, "average_data.txt", row.name = FALSE)
+  rm(rawData, tidyData)
